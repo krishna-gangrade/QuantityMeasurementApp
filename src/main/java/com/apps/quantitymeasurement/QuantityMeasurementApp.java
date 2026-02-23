a38a63e0d4fcbfd608331751d56aaf85f75c2e58
krishna <krishnagangrade2003@gmail.com>
2026-02-23 11:15:16 +0530

    [Krishna Gangrade]: added feature/UC2-InchEquality.

--- src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -10,7 +10,6 @@ public class QuantityMeasurementApp {
 		public Feet(double value) {
 			this.value = value;
 		}
-
 		@Override
 		public boolean equals(Object obj) {
 
@@ -35,14 +34,58 @@ public class QuantityMeasurementApp {
 		}
 	}
 
-// Main method to demonstrate Feet equality check
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static class Inches {
+		private final double value;
+
+		public Inches(double value) {
+			this.value = value;
+		}
+		@Override
+		public boolean equals(Object obj) {
+
+			if (this == obj) {
+				return true;
+			}
+
+			if (obj == null) {
+				return false;
+			}
+
+			if (getClass() != obj.getClass()) {
+				return false;
+			}
+
+			Inches other = (Inches) obj;
+			return Double.compare(this.value, other.value) == 0;
+		}
+
+		public double getValue() {
+			return value;
+		}
+		
+	}
+	public static void demonstrateFeetEquality(Scanner sc) {
 		double inputOne = sc.nextDouble();
 		double inputTwo = sc.nextDouble();
 		Feet f1 = new Feet(inputOne);
 		Feet f2 = new Feet(inputTwo);
-
 		System.out.println("Are equal? " + f1.equals(f2));
+	}
+	public static void demonstrateInchesEquality(Scanner sc) {
+		
+		double inputOne = sc.nextDouble();
+		double inputTwo = sc.nextDouble();
+		Inches i1 = new Inches(inputOne);
+		Inches i2 = new Inches(inputTwo);
+		System.out.println("Are equal? " + i1.equals(i2));
+        sc.close();
+	}
+	
+// Main method to demonstrate Feet equality check
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		demonstrateFeetEquality(sc);
+		demonstrateInchesEquality(sc);
+		sc.close();
 	}
 }
--- src/test/java/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
+++ src/test/java/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
@@ -1,11 +1,10 @@
-
 package com.apps.quantitymeasurement;
 
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import com.apps.quantitymeasurement.QuantityMeasurementApp.Feet;
+import com.apps.quantitymeasurement.QuantityMeasurementApp.*;
 
 public class QuantityMeasurementAppTest {
 
@@ -42,6 +41,42 @@ public class QuantityMeasurementAppTest {
     @Test
     void testFeetEquality_SameReference() {
     	QuantityMeasurementApp.Feet f1 = new Feet(1.0);
+
+        assertTrue(f1.equals(f1));
+    }
+    @Test
+    void testInchesEquality_SameValue() {
+        QuantityMeasurementApp.Inches f1 = new Inches(1.0);
+        QuantityMeasurementApp.Inches f2 = new Inches(1.0);
+
+        assertTrue(f1.equals(f2));
+    }
+
+    @Test
+    void testInchesEquality_DifferentValue() {
+        QuantityMeasurementApp.Inches f1 = new Inches(1.0);
+        QuantityMeasurementApp.Inches f2 = new Inches(2.0);
+
+        assertFalse(f1.equals(f2));
+    }
+
+    @Test
+    void testInchesEquality_NullComparison() {
+    	QuantityMeasurementApp.Inches f1 = new Inches(1.0);
+
+        assertFalse(f1.equals(null));
+    }
+
+    @Test
+    void testInchesEquality_DifferentClass() {
+    	QuantityMeasurementApp.Inches f1 = new Inches(1.0);
+
+        assertFalse(f1.equals("1.0"));
+    }
+
+    @Test
+    void testInchesEquality_SameReference() {
+    	QuantityMeasurementApp.Inches f1 = new Inches(1.0);
 
         assertTrue(f1.equals(f1));
     }
